f64039ca7ba94275a56b995d605763043d4a13a7
Tommy Cheung <tommy200401@gmail.com>
2021-10-26 22:17:22 +0800

    [EmployeeApplication][Tommy] feat: added exception

---
+++ src/main/java/com/afs/restfulapi/CompanyNotFoundException.java
@@ -0,0 +1,7 @@
+package com.afs.restfulapi;
+
+public class CompanyNotFoundException extends RuntimeException{
+    public CompanyNotFoundException(){
+        super("Company not found.");
+    }
+}

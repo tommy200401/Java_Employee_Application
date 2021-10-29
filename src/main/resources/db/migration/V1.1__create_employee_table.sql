7af9ee400cb045471252641dfa580886d6bd27d0
Tommy Cheung <tommy200401@gmail.com>
2021-10-29 08:49:25 +0800

    [EmployeeApplication][Tommy] feat: added flyway sql setup files

--- src/main/resources/db/migration/V1.1__create_employee_table.sql
+++ src/main/resources/db/migration/V1.1__create_employee_table.sql
@@ -1,5 +1,8 @@
-CREATE TABLE employee (
-    companyId int not null auto_increment primary key,
+create table employee(
+    id int not null auto_increment primary key,
     name varchar(255) not null,
-    info varchar(255) not null
-    )
+    age int,
+    salary double,
+    company_id int,
+    foreign key (company_id) references company (company_id)
+)
